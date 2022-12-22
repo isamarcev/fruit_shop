4670921eddeaa55b1aff478b1d6622e118ada3b9
isamarcev <isamarcev1994@gmail.com>
2022-12-22 16:29:29 +0200

    change init script

--- fruitshop/management/commands/create_user.py
+++ fruitshop/management/commands/create_user.py
@@ -11,11 +11,20 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='joker').exists():
             User.objects.create(username='joker', email='joker_joker@email.com')
+            print("JOKER CREATED")
+        else:
+            print("JOKER EXISTS")
         if not User.objects.filter(username="anonim").exists():
             User.objects.create(username='anonim', email='anonim@email.com')
+            print("ANONIM CREATED")
+
         if not User.objects.filter(username="admin").exists():
             admin = User.objects.create(username='admin', email='anonim@email.com')
             admin.set_password("qwerty40Req!")
             admin.save()
+            personal_account = models.PersonalAccount.objects.create(user=admin)
+            print("ADMIN CREATED")
+        else:
+            print("ADIMN EXISTS")
 
 
