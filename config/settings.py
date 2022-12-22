aafd463b9062cd106f9aac9142afab7ec6143bdf
isamarcev <isamarcev1994@gmail.com>
2022-12-22 16:14:00 +0200

    add host

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(env("DEBUG"))
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['161.35.29.183', '127.0.0.1', 'localhost']
 
 
 # Application definition
