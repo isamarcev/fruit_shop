b4dc65b509d62b0446b579014b0b1788e3c7b55b
isamarcev <isamarcev1994@gmail.com>
2022-12-22 16:39:25 +0200

    command change

--- fruitshop/management/commands/create_products.py
+++ fruitshop/management/commands/create_products.py
@@ -5,6 +5,7 @@ from fruitshop import models
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        print('PRODUCT CREATING')
         models.Product.objects.bulk_create([
             models.Product(name="Ананасы", balance=300),
             models.Product(name="Яблоки", balance=300),
--- fruitshop/management/commands/create_user.py
+++ fruitshop/management/commands/create_user.py
@@ -11,20 +11,20 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='joker').exists():
             User.objects.create(username='joker', email='joker_joker@email.com')
-            print("JOKER CREATED")
+            self.stdout.write("JOKER CREATED")
         else:
-            print("JOKER EXISTS")
+            self.stdout.write("JOKER EXISS")
         if not User.objects.filter(username="anonim").exists():
             User.objects.create(username='anonim', email='anonim@email.com')
-            print("ANONIM CREATED")
+            self.stdout.write("ANONI< CREATED")
 
         if not User.objects.filter(username="admin").exists():
             admin = User.objects.create(username='admin', email='anonim@email.com')
             admin.set_password("qwerty40Req!")
             admin.save()
             personal_account = models.PersonalAccount.objects.create(user=admin)
-            print("ADMIN CREATED")
+            self.stdout.write("ADMON CREATED")
         else:
-            print("ADIMN EXISTS")
+            self.stdout.write("ADMIN EXIRX")
 
 
