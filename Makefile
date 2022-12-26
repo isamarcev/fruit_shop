94ab7dd98d80fb8898179e7859541566ad5cd0af
isamarcev <isamarcev1994@gmail.com>
2022-12-26 23:05:15 +0200

    add command to daphne docker command

--- Makefile
+++ Makefile
@@ -16,12 +16,12 @@ beatup:
 flower:
 	celery -A config flower  --address=127.0.0.1 --port=5566
 startup:
-	python manage.py makemigrations
-	python manage.py migrate
+	#python manage.py makemigrations
+	#python manage.py migrate
 	python manage.py collectstatic --no-input
-	python manage.py create_products
-	python manage.py create_user
-	daphne config.asgi:application
+	#python manage.py create_products
+	#python manage.py create_user
+	daphne -b 0.0.0.0 -p 8000 config.asgi:application
 	#celery -A config worker -Q warehouse,celery
 	#celery -A config beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
 down:
--- fruitshop/management/commands/create_products.py
+++ fruitshop/management/commands/create_products.py
@@ -6,12 +6,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print('PRODUCT CREATING')
-        models.Product.objects.bulk_create([
-            models.Product(name="Ананасы", balance=300),
-            models.Product(name="Яблоки", balance=300),
-            models.Product(name="Бананы", balance=300),
-            models.Product(name="Апельсины", balance=300),
-            models.Product(name="Абрикосы", balance=300),
-            models.Product(name="Киви", balance=300),
-        ])
+        if not models.Product.objects.all().exists():
+            models.Product.objects.bulk_create([
+                models.Product(name="Ананасы", balance=300),
+                models.Product(name="Яблоки", balance=300),
+                models.Product(name="Бананы", balance=300),
+                models.Product(name="Апельсины", balance=300),
+                models.Product(name="Абрикосы", balance=300),
+                models.Product(name="Киви", balance=300),
+            ])
+        else:
+            self.stdout("Products Already created")
 
